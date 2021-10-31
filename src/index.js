0a17df6de73a5aa79d006fc05027a40154a038da
Alan Chiu <ms0372631@gmail.com>
2021-10-30 19:10:09 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 
 import Game from "./scripts/game";
+import GameView from "./scripts/game_view";
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello World");
-  const main = document.getElementById("main");
-  new Game(main);
+
+window.addEventListener('DOMContentLoaded', function(event) {
+  const canvasEl = document.getElementById('canvas');
+  const startButton = document.getElementById("start-btn");
+  const ctx2d = canvasEl.getContext('2d');
+  const game = new Game();
+  const game_view = new GameView(game, ctx2d);
+  
+  game_view.start();
 });
+
+startBtn.classList.remove("hidden");
